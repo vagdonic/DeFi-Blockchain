66e674f775f1651029d35a383fdd43afd1167178
vagdonic <manavvg@gmail.com>
2021-06-29 10:25:28 +0530

    4. Deploying ERC20 token

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -8,7 +8,7 @@ contract Migrations {
   modifier restricted() {
     require(
       msg.sender == owner,
-      "This function is restricted to the contracts owner"
+      "This function is restricted to the contract'sdjnsdjnsjdnsdjnsdjnsdjnjsdnjs owner"
     );
     _;
   }
